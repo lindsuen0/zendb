ee06751fa2b62bd5cedce5c385b925fdff818776
LindSuen <lindsuen@foxmail.com>
2023-12-26 21:28:47 +0800

    feat: define the TCP port: 4780

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log"
+	"net"
+
 	"github.com/lindsuen0/zendb/util/config"
 	"github.com/lindsuen0/zendb/util/db"
 )
@@ -11,5 +14,25 @@ func init() {
 }
 
 func main() {
+	listener, err := net.Listen("tcp", "127.0.0.1:4976")
+	if err != nil {
+		log.Fatalln("Error listening: ", err.Error())
+	}
+	defer listener.Close()
 
+	log.Println("Server started. Listening on port 4976...")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatalln("Error accepting connection: ", err.Error())
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	// TODO
 }
